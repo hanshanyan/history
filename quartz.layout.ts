37d87aa67383e8ebd948298b7f338fadad88031d
hanshanyan <liwenqingtu@gmail.com>
2026-07-26 20:10:19 +0800

    fix: 探索点文件夹即展开/收起；左侧栏内部滚动可展开；.center 改 border-box 左右对称留白

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,7 +38,7 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({ folderClickBehavior: "collapse" }),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
     Component.Graph(),
